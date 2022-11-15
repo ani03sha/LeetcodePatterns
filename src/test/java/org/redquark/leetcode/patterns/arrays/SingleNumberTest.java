a867ed6c8af8c4667dc5b628752f30f8c7ab99e2
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-11-15 23:08:40 +0530

    Added problem - Single Number

---
+++ src/test/java/org/redquark/leetcode/patterns/arrays/SingleNumberTest.java
@@ -0,0 +1,23 @@
+package org.redquark.leetcode.patterns.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SingleNumberTest {
+
+    private final SingleNumber testObject = new SingleNumber();
+
+    @Test
+    public void testSingleNumber() {
+        int[] nums = new int[]{2, 2, 1};
+        assertEquals(1, testObject.singleNumber(nums));
+
+        nums = new int[]{4, 1, 2, 1, 2};
+        assertEquals(4, testObject.singleNumber(nums));
+
+        nums = new int[]{1};
+        assertEquals(1, testObject.singleNumber(nums));
+    }
+
+}
