960ddb29c810c5ce9f13c96c69380631bf17ff24
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-12-26 19:24:32 +0530

    Added problem - Implement Trie (Prefix Tree)

---
+++ src/test/java/org/redquark/leetcode/patterns/design/ImplementTrieTest.java
@@ -0,0 +1,21 @@
+package org.redquark.leetcode.patterns.design;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.redquark.leetcode.patterns.design.ImplementTrie.Trie;
+
+public class ImplementTrieTest {
+
+    @Test
+    public void test() {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        assertTrue(trie.search("apple"));
+        assertFalse(trie.search("app"));
+        assertTrue(trie.startsWith("app"));
+        trie.insert("app");
+        assertTrue(trie.search("app"));
+    }
+}
